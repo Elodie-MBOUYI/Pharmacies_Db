26540a17600edf238ad2f4ae4a6940d4cd1d9059
Elod-cpu <oulendoeloise@gmail.com>
2022-11-24 03:47:33 +0100

    Bdd

--- PharmaDb.sql
+++ PharmaDb.sql
@@ -1,82 +1,86 @@
 CREATE DATABASE pharmacies;
 
--- Objet utilisateur
+-- Objet utilisateur et attributs
 
 CREATE TABLE `utilisateur` (
-  `id_Utilisateur` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  `nom` varchar(100) NOT NULL,
-  `prenom` varchar(75) NOT NULL,
-  `Adresse` int(50) NOT NULL,
+  `id_Utilisateur` int PRIMARY KEY AUTO_INCREMENT,
+  `nom` varchar(150) NOT NULL,
+  `adresse` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL
 ) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
--- Objet liste des pharmacies de Port-Gentil
 
-CREATE TABLE `Pharmacies` (
-  `id_Pharmacies` int PRIMARY KEY AUTO_INCREMENT,
-  `nom_Pharmacies` int(50) NOT NULL,
-  `id_Utilisateur` int(50) NOT NULL,
+-- Objet  pharmacie de  garde à Port-Gentil et attributs
+
+CREATE TABLE `pharmgard` (
+  `id_pharmgard` int PRIMARY KEY AUTO_INCREMENT,
+  `nom_pharmgard` varchar(25) NOT NULL,
+  `telephone` varchar(10) NOT NULL
+) ENGINE=Innodb DEFAULT CHARSET=utf8;
+
+
+-- Objet  pharmacie sollicitée et attributs
+
+CREATE TABLE `pharmacies` (
+  `id_PharmacieChoisie` int PRIMARY KEY AUTO_INCREMENT,
+  `id_pharmgard` int(9) NOT NULL,
+  `PharmacieChoisie` varchar(300) NOT NULL,
+  `produits` varchar(300) NOT NULL,
+  FOREIGN KEY (`id_pharmgard`) REFERENCES pharmgard(`id_pharmgard`),
+  KEY `produits`(`produits`)
+) ENGINE=Innodb DEFAULT CHARSET=utf8;
+
+-- Objet  produits disponible et attributs
+
+CREATE TABLE `produits` (
+  `id_produits` int PRIMARY KEY AUTO_INCREMENT,
+   `propriétés`  varchar (20) NOT NULL,
+   `produits`  varchar (20) NOT NULL,
+   `description`  varchar (20) NOT NULL,
+   `prix` float NOT NULL, 
+   FOREIGN KEY (`produits`) REFERENCES pharmacies(`produits`)
+) ENGINE=Innodb DEFAULT CHARSET=utf8;
+
+-- Objet  requête directe vers la pharmacie partenaire
+
+CREATE TABLE `requete` (
+  `id_requete` int PRIMARY KEY AUTO_INCREMENT,
+  `id_Utilisateur` int(9) NOT NULL,
+  `requete` varchar(10) NOT NULL,
   FOREIGN KEY (`id_Utilisateur`) REFERENCES utilisateur(`id_Utilisateur`)
 ) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
--- Objet pharmacie programée de garde
 
-CREATE TABLE `PharmaGarde` (
-  `id_PharmaGarde` int PRIMARY KEY AUTO_INCREMENT,
-  `nom_Pharma` int(50) NOT NULL,
-  `Adresse` varchar(50) NOT NULL,
+-- Objet  redirection directe vers les services en ligne de la pharmacie partenaire
+*
+CREATE TABLE `redirection` (
+  `id_redirection` int PRIMARY KEY AUTO_INCREMENT,
+  `nom_pharmacie` varchar(300) NOT NULL,
+   `telephone` int(50) NOT NULL,
+  `nom_application` varchar(50) NOT NULL,
   `id_Utilisateur` int(50) NOT NULL,
-  `Appel` varchar(50) NOT NULL,
-  `What's app` varchar(50) NOT NULL,
-  FOREIGN KEY (`id_Utilisateur`) REFERENCES utilisateur(`id_Utilisateur`)
+   FOREIGN KEY (`id_Utilisateur`) REFERENCES utilisateur(`id_Utilisateur`)
 ) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
--- Objet liste des produits pharmaceutiques.Toutes catégories confondues
+-- insertion des valeurs dans la table produits
 
-CREATE TABLE `Produits` (
-  `id_Produits` int PRIMARY KEY AUTO_INCREMENT,
-  `nom_Produits` int(50) NOT NULL,
-  `Catégorie` varchar(50) NOT NULL,
-  `Description` varchar(50) NOT NULL,
-  `id_Utilisateur` int(50) NOT NULL,
-  `Prix` varchar(50) NOT NULL,
-  FOREIGN KEY (`id_Utilisateur`) REFERENCES utilisateur(`id_Utilisateur`)
-) ENGINE=Innodb DEFAULT CHARSET=utf8;
+INSERT INTO `produits` (`id_produits`, `propriétés`, `produits`, `description`,`prix`,) VALUES
+(NULL, 'Propriété', 'Doliprane 1000mg',  'xjkllllmmm',  '4500 Fcfa')
+(NULL, 'Propriété', 'Doliprane 1000mg',  'xjkllllmmm',  '4500 Fcfa')
+(NULL, 'Propriété', 'Doliprane 1000mg',  'xjkllllmmm',  '4500 Fcfa')
+(NULL, 'Propriété', 'Doliprane 1000mg',  'xjkllllmmm',  '4500 Fcfa')
 
--- Objet des éventuels commandes des clients
+-- insertion des valeurs dans la table pharmacie de garde
 
-CREATE TABLE `Commandes` (
-  `id_Commandes` int PRIMARY KEY AUTO_INCREMENT,
-  `id_Produits` int (20) NOT NULL,
-  `quantité` int(50) NOT NULL,
-  `date_commande` varchar(50) NOT NULL,
-  `heure_récupération` varchar(50) NOT NULL,
-  `id_Utilisateur` int(50) NOT NULL,
-) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
--- Objet liste d'achats effectués
+INSERT INTO `` (`id_Pharmgard`, `nom_pharmgard`, `téléphone`) VALUES
+(NULL, 'Pharmacie Centrale','066232114')
+(NULL, 'Pharmacie Centrale','066232114')
+(NULL, 'Pharmacie Centrale','066232114')
 
-CREATE TABLE `Achat` (
-  `id_Achat` int PRIMARY KEY AUTO_INCREMENT,
-  `date_Achat` varchar(50) NOT NULL,
-  `id_Utilisateur` int(50) NOT NULL,
-  `nom_Produits` varchar(50) NOT NULL,
-  `quantité` varchar(50) NOT NULL,
-) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
--- Objet facture
 
-CREATE TABLE `Facture` (
-  `id_Facture` int PRIMARY KEY AUTO_INCREMENT,
-  `id_Utilisateur` int(50) NOT NULL,
-  `id_Achat` varchar(50) NOT NULL,
-  `somme` varchar(50) NOT NULL,
-) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
--- Objet Ticket_Réservation
 
-CREATE TABLE `Réservation` (
-  `id_Réservation` int PRIMARY KEY AUTO_INCREMENT,
-  `id_Utilisateur` int(50) NOT NULL,
-  `id_Commande` int(50) NOT NULL,
-  `Somme à payer` varchar(50) NOT NULL
-) ENGINE=Innodb DEFAULT CHARSET=utf8;
+
+
